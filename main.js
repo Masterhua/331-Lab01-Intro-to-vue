81f59379fecfcf27c6882d9530273ce26d115c5f
master.hua <465228865@qq.com>
2024-10-22 21:32:00 +0800

    Until task 4.9

--- main.js
+++ main.js
@@ -6,11 +6,17 @@ createApp({
         const description = ref('This is a Boots');
         const image = ref('./assets/images/socks_green.jpg');
         const productLink = ref('https://www.camt.cmu.ac.th');
+        const inStock = ref(false);
+        const inventory  = ref(1);
+        const onSale = ref(true);
         return {
             product,
             description,
             image,
             productLink,
+            inStock,
+            inventory,
+            onSale
         };
     }
 }).mount('#app');

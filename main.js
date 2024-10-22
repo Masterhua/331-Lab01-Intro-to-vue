77dbf4b57cbc8a3f774d252a84a3d6c45a17a3da
master.hua <465228865@qq.com>
2024-10-22 22:18:02 +0800

    Until task 5.5

--- main.js
+++ main.js
@@ -9,6 +9,22 @@ createApp({
         const inStock = ref(false);
         const inventory  = ref(1);
         const onSale = ref(true);
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234, color: 'green' },
+            { id: 2235, color: 'blue' }
+        ]);
+        
+        const sizes = ref([
+            { id: 1001, size: 'S' },
+            { id: 1002, size: 'M' },
+            { id: 1003, size: 'L' }
+        ]);
+
         return {
             product,
             description,
@@ -16,7 +32,10 @@ createApp({
             productLink,
             inStock,
             inventory,
-            onSale
+            onSale,
+            details,
+            variants,
+            sizes
         };
     }
 }).mount('#app');
